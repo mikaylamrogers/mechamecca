52b0a3c1c18556c97deae8b966ef86fb39de7919
Mikayla Mayumi <58568794+mikaylamrogers@users.noreply.github.com>
2023-01-21 11:15:42 -0500

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,18 @@
+// background gradient source code: https://codepen.io/lsammarco/pen/waMpJw
+$(document).mousemove(function(event) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    
+    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #d0e9f2, #86a3bf)');
+  });
+
+  const scrollContainer = document.querySelector("main");
+
+  scrollContainer.addEventListener("wheel", (evt) => {
+      evt.preventDefault();
+      scrollContainer.scrollLeft += evt.deltaY;
+  });
+  
